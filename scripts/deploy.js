ea258db3c8e93a390efcc3d402a6469f47f2ad36
hmanukyan <hmanukyan@vmware.com>
2022-04-27 14:12:53 +0400

    Add deploy NFTFactory contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,20 +9,30 @@ const fs = require("fs");
 async function main() {
   const Greeter = await hre.ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const NFTFactory = await hre.ethers.getContractFactory("NFTFactory");
+  const nftFactory = await NFTFactory.deploy("Hovo", "Hakob!");
 
   await greeter.deployed();
+  await nftFactory.deployed();
 
 
     console.log("greeter.address is a: ", greeter.address);
 
 
-    const data = {
+    const dataGreeter = {
         address: greeter.address,
         abi: JSON.parse(greeter.interface.format('json'))
     };
-    fs.writeFileSync('./Token.deployed.json', JSON.stringify(data));
+
+    const dataNFTFactory = {
+        address: nftFactory.address,
+        abi: JSON.parse(nftFactory.interface.format('json'))
+    };
+    fs.writeFileSync('./src/Token.deployed.json', JSON.stringify(dataGreeter));
+    fs.writeFileSync('./src/NFTFactory.deployed.json', JSON.stringify(dataNFTFactory));
 
   console.log("Greeter deployed to:", greeter.address);
+  console.log("NFTFactory deployed to:", nftFactory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
