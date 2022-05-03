8df74b6deb480c912b2e77e063d9a71e2c6a2b0f
hmanukyan <hmanukyan@vmware.com>
2022-05-03 11:49:37 +0400

    Add burnable functionality

--- contracts/NFTFactory.sol
+++ contracts/NFTFactory.sol
@@ -3,8 +3,9 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract NFTFactory is ERC721Enumerable, Ownable {
+contract NFTFactory is ERC721Enumerable, Ownable, ERC721Burnable {
     string public baseURI;
     uint256 public cost = 0.01 ether;
 
