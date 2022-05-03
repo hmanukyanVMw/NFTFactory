8a1ba5d77320005a9bd9618bf9b17ccce704e8a5
hmanukyan <hmanukyan@vmware.com>
2022-05-03 16:22:55 +0400

    deploy js deploy only NFTFactory smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,31 +7,29 @@ const hre = require("hardhat");
 const fs = require("fs");
 
 async function main() {
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // const Greeter = await hre.ethers.getContractFactory("Greeter");
+  // const greeter = await Greeter.deploy("Hello, Hardhat!");
   const NFTFactory = await hre.ethers.getContractFactory("NFTFactory");
   const nftFactory = await NFTFactory.deploy("Hovo", "Hakob!");
 
-  await greeter.deployed();
+  // await greeter.deployed();
   await nftFactory.deployed();
 
 
-    console.log("greeter.address is a: ", greeter.address);
-
-
-    const dataGreeter = {
-        address: greeter.address,
-        abi: JSON.parse(greeter.interface.format('json'))
-    };
+    // console.log("greeter.address is a: ", greeter.address);
+    // const dataGreeter = {
+    //     address: greeter.address,
+    //     abi: JSON.parse(greeter.interface.format('json'))
+    // };
 
     const dataNFTFactory = {
         address: nftFactory.address,
         abi: JSON.parse(nftFactory.interface.format('json'))
     };
-    fs.writeFileSync('./src/Token.deployed.json', JSON.stringify(dataGreeter));
+    // fs.writeFileSync('./src/Token.deployed.json', JSON.stringify(dataGreeter));
     fs.writeFileSync('./src/NFTFactory.deployed.json', JSON.stringify(dataNFTFactory));
 
-  console.log("Greeter deployed to:", greeter.address);
+  // console.log("Greeter deployed to:", greeter.address);
   console.log("NFTFactory deployed to:", nftFactory.address);
 }
 
